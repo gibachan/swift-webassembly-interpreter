713a2eb3e696b88c1133ff5c769845e291214f6c
gibachan <childhoodend.tk@gmail.com>
2022-12-04 10:18:16 +0900

    Init package

---
+++ Sources/swift-webassembly-interpreter/swift_webassembly_interpreter.swift
@@ -0,0 +1,6 @@
+public struct swift_webassembly_interpreter {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/swift-webassembly-interpreterTests/swift_webassembly_interpreterTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import swift_webassembly_interpreter
+
+final class swift_webassembly_interpreterTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swift_webassembly_interpreter().text, "Hello, World!")
+    }
+}
