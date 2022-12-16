ea3179453f4d7395538a6457a258cb023cbef9d9
gibachan <childhoodend.tk@gmail.com>
2022-12-17 06:50:24 +0900

    Update demo

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # swift-webassembly-interpreter
 
 https://webassembly.github.io/spec/core/
+
+## Usage
+
+```
+$ swift run WebAssemblyInterpreterDemo ./Tests/WebAssemblyInterpreterTests/Resources/wasms/is_prime.wasm 97
+...
+97 is a prime number.
+```
--- Sources/WebAssemblyInterpreterDemo/Demo.swift
+++ Sources/WebAssemblyInterpreterDemo/Demo.swift
@@ -4,30 +4,36 @@ import WebAssemblyInterpreter
 @main
 public struct Demo {
     public static func main() {
-        guard CommandLine.arguments.count == 2 else {
+        guard CommandLine.arguments.count == 3 else {
             print("File path is not specified.")
             return
         }
 
         let filePath = CommandLine.arguments[1]
+        guard let inputValue = Int32(CommandLine.arguments[2]) else {
+            print("Input value is not specified.")
+            return
+        }
+        
         do {
             let decoder = try WasmDecoder(filePath: filePath)
             let wasm = try decoder.invoke()
             
             let runtime = Runtime()
             var result: Value?
-            let hostEnvironment = HostEnvironment()
-            hostEnvironment.addCode(name: "print_string") { arguments in
-                print("print_string result=\(hostEnvironment.memory.data)")
-                return []
-            }
-            hostEnvironment.addGlobal(name: "start_string", value: .i32(0))
-            let moduleInstance = runtime.instanciate(module: wasm.module,
-                                                     hostEnvironment: hostEnvironment)
+            let moduleInstance = runtime.instanciate(module: wasm.module)
             try runtime.invoke(moduleInstance: moduleInstance,
-                               functionName: "helloworld", arguments: [], result: &result)
-            
-            print("[Succeeded] result: \(result.debugDescription)")
+                               functionName: "is_prime", arguments: [.i32(inputValue)], result: &result)
+            switch result {
+            case let .some(.i32(result)):
+                if result == 0 {
+                    print("\(inputValue) is not a prime number.")
+                } else {
+                    print("\(inputValue) is a prime number.")
+                }
+            default:
+                print("Unexpected error occured.")
+            }
         } catch {
             print("Failed to parse wasm: \(filePath)")
             print("\(error) : \(error.localizedDescription)")
