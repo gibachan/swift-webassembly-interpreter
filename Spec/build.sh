09bf9ad3279e82306ec57ddb3d155600a0c7f999
gibachan <childhoodend.tk@gmail.com>
2023-01-17 22:06:38 +0900

    Fix running spec tests

--- Spec/build.sh
+++ Spec/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -eu
-rm -rf ../Tests/Spec/Resources/build
-mkdir ../Tests/Spec/Resources/build
-cp ./core/*.wast ../Tests/Spec/Resources/build
+rm -rf ./Tests/Spec/Resources/build
+mkdir ./Tests/Spec/Resources/build
+cp ./Spec/core/*.wast ./Tests/Spec/Resources/build
 
-for file in $(ls ../Tests/Spec/Resources/build); do
-  wast2json ../Tests/Spec/Resources/build/$file -o ../Tests/Spec/Resources/build/$file.json
+for file in $(ls ./Tests/Spec/Resources/build); do
+  wast2json ./Tests/Spec/Resources/build/$file -o ./Tests/Spec/Resources/build/$file.json
 done
